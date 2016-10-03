6897048d0a244e73b6cc5385f37b6cd64ce5cc82
annejones817 <annejones817@gmail.com>
2016-10-02 18:22:46 -0700

    Initial Commit - basic functionality

---
+++ README.md
@@ -0,0 +1,2 @@
+Description: Anne Jones' Thinkful Tube project. This site allows a user to search for YouTube videos. 
+Link to live page: 
---
+++ app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(event){
+	var baseURL = 'https://www.googleapis.com/youtube/v3/search';
+
+	function getResultsFromApi(search, callback) {
+		var query = {
+			q: search, 
+			part: 'snippet',
+			key: 'AIzaSyCoPg8hHzaPJWNZUb-NCk9lLeZZ1qosBRk'
+		}
+		$.getJSON(baseURL, query, callback);
+	}
+
+	function displayResults(data) {
+		$('h1').text('Results');
+		$('form').remove();
+		var resultsHTML = '';
+		if (data.items) {
+			data.items.forEach(function(item) {
+				resultsHTML += '<div class="result-item"><h2>' + item.snippet.title + 
+				'</h2><a href="https://www.youtube.com/watch?v=' + item.id.videoId +
+				'"><img src="' + item.snippet.thumbnails.medium.url + '"></a></div>'})
+			 } else {
+				resultsHTML = '<p>Sorry, there are no vidoes that match your search</p>'
+			}
+		$('main').append(resultsHTML);
+	}
+
+	$('.youtube-search').submit(function(event){
+		event.preventDefault();
+		var search = $(this).find('input').val();
+		getResultsFromApi(search, displayResults);
+	});
+
+});
