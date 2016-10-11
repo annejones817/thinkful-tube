f5227ddfe468476db9ca4198a6801d6fde8d9561
annejones817 <annejones817@gmail.com>
2016-10-11 14:41:37 -0700

    Updated to use OOP

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(document).ready(function(event){
-	var baseURL = 'https://www.googleapis.com/youtube/v3/search';
+	
 
 	function getResultsFromApi(search, callback) {
+		var baseURL = 'https://www.googleapis.com/youtube/v3/search';
 		var query = {
 			q: search, 
 			part: 'snippet',
@@ -10,18 +11,48 @@ $(document).ready(function(event){
 		$.getJSON(baseURL, query, callback);
 	}
 
-	function displayResults(data) {
+	var videoResult = function(title, id, imageSource, channel) {
+		this.title=title; 
+		this.id=id; 
+		this.imageSource=imageSource; 
+		this.videoUrl='https://www.youtube.com/watch?v=' + id; 
+		this.channelUrl='https://www.youtube.com/channel/' + channel
+
+	}
+
+	function createVideoResults(data) {
+		var videoResults = [];
+		for (var i=0; i<data.items.length; i++) {
+			 videoResults[i] = new videoResult(data.items[i].snippet.title, 
+				data.items[i].id.videoId,
+				data.items[i].snippet.thumbnails.medium.url, 
+				data.items[i].snippet.channelId);
+		}
+		console.log(videoResults);
+		displayResults(videoResults);
+	}
+
+	function displayResults(videoResults) {
 		$('h1').text('Results');
 		$('form').remove();
 		var resultsHTML = '';
-		if (data.items) {
+		if (videoResults.length) {
+			videoResults.forEach(function(item){
+			resultsHTML += '<div class="result-item"><h2>' + item.title + 
+				'</h2><a target="_blank" href="' + item.videoURL +
+				'"><img src="' + item.imageSource 
+				+ '"></a><p class="channel-link"><a target="_blank" href="'
+				+ item.channelUrl +'">See More Videos From This Channel</a></p></div>'	
+			});
+		}
+		/*if (data.items) {
 			data.items.forEach(function(item) {
 				resultsHTML += '<div class="result-item"><h2>' + item.snippet.title + 
 				'</h2><a target="_blank" href="https://www.youtube.com/watch?v=' + item.id.videoId +
 				'"><img src="' + item.snippet.thumbnails.medium.url 
 				+ '"></a><p class="channel-link"><a target="_blank" href="https://www.youtube.com/channel/'
 				+ item.snippet.channelId +'">See More Videos From This Channel</a></p></div>'});
-			 } else {
+			 }*/ else {
 				resultsHTML = '<p>Sorry, there are no vidoes that match your search</p>'
 			}
 		$('main').append(resultsHTML);
@@ -30,7 +61,7 @@ $(document).ready(function(event){
 	$('.youtube-search').submit(function(event){
 		event.preventDefault();
 		var search = $(this).find('input').val();
-		getResultsFromApi(search, displayResults);
+		getResultsFromApi(search, createVideoResults);
 	});
 
 });
